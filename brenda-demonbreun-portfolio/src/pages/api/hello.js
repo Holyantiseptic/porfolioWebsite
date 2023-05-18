0fdef060324bbb9ea1b8a883dd124b9f190abf40
William McGiffert <williammcgiffert@Williams-MacBook-Pro.local>
2023-05-18 09:19:05 -0500

    cleaned up Top Links & Changed API to get books template

--- brenda-demonbreun-portfolio/src/pages/api/hello.js
+++ brenda-demonbreun-portfolio/src/pages/api/hello.js
@@ -1,5 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(
+    [
+      {
+        ID: 1, 
+        BookTitle: 'Brenda-Book-1',
+        AmazonAffiLink: 'www.amazon.com/Book1',
+      },
+      { 
+        ID: 2,
+        BookTitle: 'Brenda-Book-2',
+        AmazonAffiLink: 'www.amazon.com/Book2',
+      },
+      { 
+        ID: 3,
+        BookTitle: 'Brenda-Book-3',
+        AmazonAffiLink: 'www.amazon.com/Book3',
+      },
+    ]
+  )
 }
