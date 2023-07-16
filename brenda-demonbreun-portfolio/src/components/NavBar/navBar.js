f0a36a1239a1c473f895447097d40cdb08f31a7e
William McGiffert <williammcgiffert@williams-mbp.lan>
2023-07-16 13:48:25 -0500

    Edited Nav bar styling to better match the layout also removed the description section

--- brenda-demonbreun-portfolio/src/components/NavBar/navBar.js
+++ brenda-demonbreun-portfolio/src/components/NavBar/navBar.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from './NavBar.module.css';
+import Image from 'next/image';
 
 const NavBar = () => {
     return (
         <nav className={styles.nav}>
+            <div className={styles.image}>
+                <Image
+                    className={styles.logo}
+                    src={"/static/Holy-Antiseptic.jpeg"}
+                    alt="Holy-Antiseptic-Logo"
+                    width={300}
+                    height={200}
+                    priority
+                />
+            </div>
             <ul className={styles.ul}>
                 <li className={styles.li}>
                     <Link
