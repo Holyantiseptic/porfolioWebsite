2361e06b8d7987f94581b8a28939035b05c126ad
William McGiffert <williammcgiffert@williams-mbp.lan>
2023-07-16 14:09:19 -0500

    Edited Nav bar styling to better match the layout also removed the description section

--- brenda-demonbreun-portfolio/src/pages/contact.js
+++ brenda-demonbreun-portfolio/src/pages/contact.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import NavBar from '../components/NavBar/navBar'
+import NavBar from '../components/NavBar/NavBar'
 import styles from '../styles/Home.module.css'
 
 
